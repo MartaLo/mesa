acd328a5d070e5eba0e85154d779f09620b37039
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2015-12-15 12:51:48 +0100

    i965/wm: use proper API buffer size for the surfaces.
    Commit 5bb5eeea fix a bug indicating that the surfaces should have the
    API buffer size. Hovewer it picked the wrong value.
    
    This patch adds a new variable, which takes into account
    glBindBufferRange() values. Fixes the following regressions:
    
    ES31-CTS.shader_storage_buffer_object.advanced-unsizedArrayLength-cs-std430-vec-bindrangeOffset
    ES31-CTS.shader_storage_buffer_object.advanced-unsizedArrayLength-cs-std430-vec-bindrangeSize
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/mesa/main/bufferobj.c
+++ src/mesa/main/bufferobj.c
@@ -43,7 +43,7 @@
 #include "glformats.h"
 #include "texstore.h"
 #include "transformfeedback.h"
-
+#include "macros.h"
 
 /* Debug flags */
 /*#define VBO_DEBUG*/
@@ -2838,6 +2838,7 @@ bind_buffer_range_uniform_buffer(struct gl_context *ctx,
 
    _mesa_reference_buffer_object(ctx, &ctx->UniformBuffer, bufObj);
    bind_uniform_buffer(ctx, index, bufObj, offset, size, GL_FALSE);
+   bufObj->BufferRangeSize = MIN2(size, bufObj->Size);
 }
 
 /**
@@ -2873,6 +2874,7 @@ bind_buffer_range_shader_storage_buffer(struct gl_context *ctx,
 
    _mesa_reference_buffer_object(ctx, &ctx->ShaderStorageBuffer, bufObj);
    bind_shader_storage_buffer(ctx, index, bufObj, offset, size, GL_FALSE);
+   bufObj->BufferRangeSize = MIN2(size, bufObj->Size);
 }
 
 /**
